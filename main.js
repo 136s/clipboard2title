d74486738e80c97878c010f9b19b4abe0b83ecef
136s <47920723+136s@users.noreply.github.com>
2020-04-07 11:23:19 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+﻿$(function() {
+	t = document.createElement("input");
+	document.body.appendChild(t);
+	t.focus();
+	document.execCommand("paste");
+	document.title = t.value;
+	console.log(t.value);
+	document.body.removeChild(t);
+});
